2d34edebda51a61b64acc1e69bbf79c36cc33d15
Whien <sal95610@gmail.com>
2016-07-23 10:44:59 +0800

    added es7 decorators

--- test/react-facebook-login-hoc-spec.js
+++ test/react-facebook-login-hoc-spec.js
@@ -1,4 +1,30 @@
 import expect from 'expect'
 import TestUtils from 'react-addons-test-utils'
-
+import React, { PropTypes, Component } from 'react'
 import LoginHOC from '../src/index'
+
+describe('React', () => {
+  describe('LoginHOC', () => {
+
+    const configureLoginProps = {
+      scope: 'public_profile',
+      xfbml: false,
+      cookie: false,
+      version: 2.6,
+      language: 'en_US',
+      appId: 'Your APP ID'
+    }
+
+    @LoginHOC(configureLoginProps)
+    class App extends Component {
+      render() {
+        return (
+          <div>
+            <button onClick={ this.checkLoginState.bind(this) }>Get Facebook Login Status</button>
+            <button onClick={ this.loginFacebook.bind(this) }>Facebook Login </button>
+          </div>
+        );
+      }
+    }
+  })
+})
