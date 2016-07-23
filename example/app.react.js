75475815d2bb79d9c164c3a2bce49ff5a6d8d509
Whien <sal95610@gmail.com>
2016-07-23 13:01:29 +0800

    fixed example

--- example/app.react.js
+++ example/app.react.js
@@ -8,32 +8,40 @@ const configureLoginProps = {
   cookie: false,
   version: 2.6,
   language: 'en_US',
-  appId: 'Your APP ID'
+  appId: '488387194689361'
 }
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
 
     this.status = this.props.fb.status
     this.login = this.props.fb.login
+    this.logout = this.props.fb.logout
   }
   getStatus(response) {
     if (response.authResponse) {
-      this.responseApi(response.authResponse);
+      this.responseApi.call(this, response.authResponse)
     }
   }
+  responseApi(res) {
+    console.log('token:', res.accessToken)
+  }
   checkLoginState() {
-    this.status(this.getStatus)
+    this.status(this.getStatus.bind(this))
   };
   loginFacebook() {
-    this.login(this.getStatus)
+    this.login(this.getStatus.bind(this))
+  }
+  logoutFacebook() {
+    this.logout()
   }
   render() {
     return (
       <div>
         <button onClick={ this.checkLoginState.bind(this) }>Get Facebook Login Status</button>
-        <button onClick={ this.loginFacebook.bind(this) }>Facebook Login </button>
+        <button onClick={ this.loginFacebook.bind(this) }>Facebook Login</button>
+        <button onClick={ this.logoutFacebook.bind(this) }>Facebook Logout</button>
       </div>
     );
   }
